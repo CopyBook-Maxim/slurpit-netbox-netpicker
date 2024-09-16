0ffd7f5799b91c559176826e9bdd952b38d767b7
Philip <philipapbak@gmail.com>
2024-09-16 02:21:15 -0400

    Fixed the serializer

--- src/slurpit_netbox/api/serializers.py
+++ src/slurpit_netbox/api/serializers.py
@@ -36,8 +36,7 @@ class SlurpitInitIPAddressSerializer(serializers.ModelSerializer):
         model = SlurpitInitIPAddress
         
         fields = [
-            'id', 'url', 'display_url', 'display', 'family', 'address', 'vrf', 'tenant', 'status', 'role',
-            'assigned_object_type', 'assigned_object_id', 'assigned_object', 'nat_inside', 'nat_outside',
+            'id', 'address', 'vrf', 'tenant', 'status', 'role',
             'dns_name', 'description', 'comments', 'tags', 'custom_fields', 'created', 'last_updated',
         ]
 
@@ -45,13 +44,10 @@ class SlurpitInterfaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = SlurpitInterface
         fields = [
-            'id', 'url', 'display_url', 'display', 'device', 'vdcs', 'module', 'name', 'label', 'type', 'enabled',
-            'parent', 'bridge', 'lag', 'mtu', 'mac_address', 'speed', 'duplex', 'wwn', 'mgmt_only', 'description',
-            'mode', 'rf_role', 'rf_channel', 'poe_mode', 'poe_type', 'rf_channel_frequency', 'rf_channel_width',
-            'tx_power', 'untagged_vlan', 'tagged_vlans', 'mark_connected', 'cable', 'cable_end', 'wireless_link',
-            'link_peers', 'link_peers_type', 'wireless_lans', 'vrf', 'l2vpn_termination', 'connected_endpoints',
-            'connected_endpoints_type', 'connected_endpoints_reachable', 'tags', 'custom_fields', 'created',
-            'last_updated', 'count_ipaddresses', 'count_fhrp_groups', '_occupied',
+            'id', 'device', 'module', 'name', 'label', 'type', 'enabled',
+            'parent', 'bridge', 'mtu', 'mac_address', 'speed', 'duplex', 'description',
+            'mode', 'tags', 'custom_fields', 'created',
+            'last_updated'
         ]
 
 class SlurpitSnapshotSerializer(serializers.ModelSerializer):
@@ -85,16 +81,15 @@ class SlurpitVLANSerializer(NetBoxModelSerializer):
     class Meta:
         model = SlurpitVLAN
         fields = [
-            'id', 'url', 'display_url', 'display', 'site', 'group', 'vid', 'name', 'tenant', 'status', 'role',
-            'description', 'comments', 'l2vpn_termination', 'tags', 'custom_fields', 'created', 'last_updated',
-            'prefix_count',
+            'id', 'site', 'group', 'vid', 'name', 'tenant', 'status', 'role',
+            'description', 'comments', 'tags', 'custom_fields', 'created', 'last_updated',
         ]
 
 class SlurpitPrefixSerializer(NetBoxModelSerializer):
     class Meta:
         model = SlurpitPrefix
         fields = [
-            'id', 'url', 'display_url', 'display', 'family', 'prefix', 'site', 'vrf', 'tenant', 'vlan', 'status',
+            'id', 'family', 'prefix', 'site', 'vrf', 'tenant', 'vlan', 'status',
             'role', 'is_pool', 'mark_utilized', 'description', 'comments', 'tags', 'custom_fields',
             'created', 'last_updated', 'children', '_depth',
         ]
