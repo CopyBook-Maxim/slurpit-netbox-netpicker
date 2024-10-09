50fd35cab952a1492be7c6d0a19595b399ab64af
Philip <philipapbak@gmail.com>
2024-10-09 02:57:25 -0400

    Fixed logo image size

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='1.0.2',
+    version='1.0.4',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
