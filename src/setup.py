33ca5bedc193a5e6a36238beca455601c41c2073
Philip <philipapbak@gmail.com>
2024-10-30 05:21:21 -0400

    Added log migration

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='1.0.28',
+    version='1.0.29',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
