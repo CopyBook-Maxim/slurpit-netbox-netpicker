f269382a4e6238f23370e2d7a1c74cfe063b4b00
Philip <philipapbak@gmail.com>
2024-12-18 02:23:38 -0500

    updated version

--- src/slurpit_netbox/__init__.py
+++ src/slurpit_netbox/__init__.py
@@ -8,7 +8,7 @@ class SlurpitConfig(PluginConfig):
     name = "slurpit_netbox"
     verbose_name = "Slurp'it Plugin"
     description = "Sync Slurp'it into NetBox"
-    version = '1.0.41'
+    version = '1.0.42'
     base_url = "slurpit"   
     default_settings = {
         'DeviceType': {'model': "Slurp'it", 'slug': 'slurpit'},
